f7bc1ac15aa5663285e69b1e7b8fcb9a1fabdd52
Tomas <0358332@student.rocvantwente.nl>
2025-03-25 09:33:25 +0100

    IsPalindrome is af en werkt

---
+++ Semester6Opdrachten/Controllers/OpdrachtenController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Semester6Opdrachten.ExtensionMethods;
+
+namespace Semester6Opdrachten.Controllers
+{
+	public class OpdrachtenController : Controller
+	{
+		[HttpGet]
+		public IActionResult ExtensionMethods()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult ExtensionMethods(string inputWord)
+		{
+			ViewBag.IsPalindrome = !string.IsNullOrEmpty(inputWord) && inputWord.IsPalindrome();
+			return View((object)inputWord);
+		}
+	}
+}
---
+++ Semester6Opdrachten/ExtensionMethods/ExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿namespace Semester6Opdrachten.ExtensionMethods
+{
+	public static class ExtensionMethods
+	{
+		public static bool IsPalindrome(this string str)
+		{
+			char[] charArray = str.ToCharArray();
+			Array.Reverse(charArray);
+			return new string(charArray) == str;
+		}
+
+
+		public static DateTime AddMinutes(this DateTime date, int minutes)
+		{
+			return date.AddMinutes(minutes);
+		}
+	}
+}
