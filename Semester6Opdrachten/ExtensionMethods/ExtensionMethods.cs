d45e36646dba9aaa7a447b0fb6f98369ac87b8a2
Tomas <0358332@student.rocvantwente.nl>
2025-03-25 10:58:32 +0100

    andere methods

--- Semester6Opdrachten/ExtensionMethods/ExtensionMethods.cs
+++ Semester6Opdrachten/ExtensionMethods/ExtensionMethods.cs
@@ -1,7 +1,37 @@
-﻿namespace Semester6Opdrachten.ExtensionMethods
+﻿using System.Globalization;
+
+namespace Semester6Opdrachten.ExtensionMethods
 {
+	public enum CurrencyCountry
+	{
+		USA,
+		UK,
+		Japan,
+		Canada,
+		Australia,
+		Switzerland,
+		Euro
+	}
+
 	public static class ExtensionMethods
 	{
+		public static string ToCurrencyString(this decimal amount, CurrencyCountry country)
+		{
+			CultureInfo cultureInfo = country switch
+			{
+				CurrencyCountry.USA => new CultureInfo("en-US"),
+				CurrencyCountry.UK => new CultureInfo("en-GB"),
+				CurrencyCountry.Japan => new CultureInfo("ja-JP"),
+				CurrencyCountry.Canada => new CultureInfo("en-CA"),
+				CurrencyCountry.Australia => new CultureInfo("en-AU"),
+				CurrencyCountry.Switzerland => new CultureInfo("de-CH"),
+				CurrencyCountry.Euro => new CultureInfo("fr-FR"),
+				_ => null
+			};
+
+			return cultureInfo != null ? amount.ToString("C", cultureInfo) : amount.ToString();
+		}
+
 		public static bool IsPalindrome(this string str)
 		{
 			char[] charArray = str.ToCharArray();
@@ -9,10 +39,22 @@
 			return new string(charArray) == str;
 		}
 
-
 		public static DateTime AddMinutes(this DateTime date, int minutes)
 		{
 			return date.AddMinutes(minutes);
 		}
+
+		public static bool IsEven(this int number)
+		{
+			return number % 2 == 0;
+		}
+
+		public static string FirstCharToUpper(this string str)
+		{
+			if (string.IsNullOrEmpty(str))
+				return str;
+			return char.ToUpper(str[0]) + str.Substring(1).ToLower();
+		}
 	}
 }
+
