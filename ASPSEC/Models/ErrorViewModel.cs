15d76610daf6927d57a74afef0728247cae8611a
Tomas <0358332@student.rocvantwente.nl>
2025-03-25 11:28:46 +0100

    ASPSEC toegevoegd

---
+++ ASPSEC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPSEC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
