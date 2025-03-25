b9e1b150cb0326818300ff0c593bafb315f782ee
Tomas <0358332@student.rocvantwente.nl>
2025-03-25 11:05:15 +0100

    view en controller aangepast op nieuwe methods

--- Semester6Opdrachten/Controllers/OpdrachtenController.cs
+++ Semester6Opdrachten/Controllers/OpdrachtenController.cs
@@ -12,10 +12,38 @@ namespace Semester6Opdrachten.Controllers
 		}
 
 		[HttpPost]
-		public IActionResult ExtensionMethods(string inputWord)
+		public IActionResult CheckPalindrome(string inputWord)
 		{
 			ViewBag.IsPalindrome = !string.IsNullOrEmpty(inputWord) && inputWord.IsPalindrome();
-			return View((object)inputWord);
+			return View("ExtensionMethods", (object)inputWord);
+		}
+
+		[HttpPost]
+		public IActionResult CheckCurrency(decimal amount, CurrencyCountry country)
+		{
+			ViewBag.CurrencyExample = amount.ToCurrencyString(country);
+			return View("ExtensionMethods");
+		}
+
+		[HttpPost]
+		public IActionResult CheckEvenNumber(string inputNumber)
+		{
+			if (int.TryParse(inputNumber, out int number))
+			{
+				ViewBag.IsEven = number.IsEven();
+			}
+			else
+			{
+				ViewBag.IsEven = false; 
+			}
+			return View("ExtensionMethods");
+		}
+
+		[HttpPost]
+		public IActionResult FirstCharToUpper(string inputString)
+		{
+			ViewBag.FirstCharToUpper = inputString?.FirstCharToUpper();
+			return View("ExtensionMethods");
 		}
 	}
 }
