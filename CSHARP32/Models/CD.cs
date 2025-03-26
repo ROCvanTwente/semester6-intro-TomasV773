05d49304368572f2806218f1a9be88cc5ec96092
Tomas <0358332@student.rocvantwente.nl>
2025-03-26 10:02:02 +0100

    models en packages

---
+++ CSHARP32/Models/CD.cs
@@ -0,0 +1,12 @@
+﻿namespace CSHARP32.Models
+{
+	public class CD : Artikel
+	{
+		public int AantalLiedjes { get; set; }
+
+		public override decimal TotaleWaarde()
+		{
+			return AantalStuks * Prijs;
+		}
+	}
+}
