0508556b95bcc8262ee38432b5233dee9443fc97
Tomas <0358332@student.rocvantwente.nl>
2025-03-28 14:20:40 +0100

    connection met dashboard werkt

---
+++ SignalRDbDependency/wwwroot/js/dashboardHub.js
@@ -0,0 +1,11 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/dashboardHub").build();
+
+$(function () {
+    connection.start().then(function () {
+        alert("Verbonden met dashboardHub");
+    }).catch(function (err) {
+        return console.error(err.toString());
+    });
+});
